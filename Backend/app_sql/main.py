630a41030f318e1fdf0bd277f392f6b9964509c3
Willians <09willians@gmail.com>
2024-04-13 16:25:28 +0000

    feat/refactor: rota para buscar um user e retirada da função get_db

--- Backend/app_sql/main.py
+++ Backend/app_sql/main.py
@@ -9,7 +9,7 @@ from . import crud, models, schemas
 # from app_sql import schemas
 
 # from .database import SessionLocal, engine
-from .database import session, engine
+from .database import session, engine, get_db 
 
 # models.Base.metadata.create_all(bind=engine)
 
@@ -18,13 +18,13 @@ from .database import session, engine
 app = FastAPI()
 
 
-# Dependency
-def get_db():
-    db = session
-    try:
-        yield db
-    finally:
-        db.close()
+# # Dependency
+# def get_db():
+#     db = session
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 # def get_db():
 #     db = SessionLocal()
@@ -49,12 +49,12 @@ def create_user(user: schemas.User, db: Session = Depends(get_db)):
 #     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+# @app.get("/users/{user_id}", response_model=schemas.User)
+# def read_user(user_id: int, db: Session = Depends(get_db)):
+#     db_user = crud.get_user(db, user_id=user_id)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
 
 
 # @app.post("/users/{user_id}/items/", response_model=schemas.Products)
@@ -68,3 +68,6 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 # def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 #     items = crud.get_items(db, skip=skip, limit=limit)
 #     return items
+
+from .routes import routerUser
+app.include_router(routerUser)
