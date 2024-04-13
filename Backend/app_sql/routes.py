560965b8d72bade221ebeb9a6796e239e9f66e82
Willians <09willians@gmail.com>
2024-04-13 16:44:18 +0000

    feat: Criar user

--- Backend/app_sql/routes.py
+++ Backend/app_sql/routes.py
@@ -8,6 +8,14 @@ from . import schemas, crud
 
 routerUser = APIRouter(prefix="/users")
 
+
+@routerUser.post("/", response_model=schemas.UserBase)
+def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
 @routerUser.get("/{user_id}", response_model=schemas.UserBase)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
