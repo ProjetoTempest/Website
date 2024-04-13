b641200c5923a4e4b92a6d3d41f1353cf7e3d519
Willians <09willians@gmail.com>
2024-04-13 16:45:15 +0000

    refactor: remoção de comentários e códigos não utilizados

--- Backend/app_sql/main.py
+++ Backend/app_sql/main.py
@@ -1,46 +1,10 @@
 from fastapi import Depends, FastAPI, HTTPException
-# from sqlalchemy.orm import Session
 from sqlmodel import Session
 
-from . import crud, models, schemas
-
-# from app_sql import crud
-# from app_sql import models
-# from app_sql import schemas
-
-# from .database import SessionLocal, engine
 from .database import session, engine, get_db 
-
-# models.Base.metadata.create_all(bind=engine)
-
-
 
 app = FastAPI()
 
-
-# # Dependency
-# def get_db():
-#     db = session
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
-@app.post("/users/", response_model=schemas.UserBase)
-def create_user(user: schemas.User, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    # print(db_user)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
 
 
 # @app.get("/users/", response_model=list[schemas.User])
