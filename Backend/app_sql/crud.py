f69a3789a37500357877bd84d82d7edffd528ad1
Willians <09willians@gmail.com>
2024-05-06 01:08:29 +0000

    Feat: atualizar email do usuario

--- Backend/app_sql/crud.py
+++ Backend/app_sql/crud.py
@@ -1,17 +1,11 @@
 # from sqlalchemy.orm import Session
-from sqlmodel import Session, select, delete, and_
+from sqlmodel import Session, select, delete, update
 from . import schemas
 
 
 def get_user(db: Session, user_id: int):
     query = select(schemas.User).where(schemas.User.id == user_id)
     user_db = db.exec(query).first()
-
-    user_db = user_db.model_dump()
-
-    user_db.pop('password')
-    user_db.pop('cargotes')
-
     return user_db
 
 
@@ -31,13 +25,32 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.User):
-    db_user = schemas.User(name=user.name, email=user.email, cargotes=user.cargotes, photo=user.photo, description=user.description, password=user.password)
 
+    db_user = schemas.User(name=user.name, email=user.email, cargotes=user.cargotes, photo=user.photo, description=user.description, password=user.password)
+    # user = str(user.model_dump()).replace("{", "").replace("}", "")
+    # print("#############################################1")
+    # # print()
+    # print(user)
+    # print("#############################################2")
+    # db_user = schemas.User(user)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
 
     return db_user
+
+
+# Colocar senha para verificação
+def update_email_user(db: Session, new_email:str, old_email):
+    query = update(schemas.User).where(schemas.User.email == old_email).values(email=new_email)
+    user_db = db.exec(query)
+    # db.add(user_db)
+    db.commit()
+    # db.refresh(user_db)
+    return user_db
+
+def update_all_infos_user(db: Session, user:schemas.User):
+    pass
 
 
 def delete_user_by_email_password(db: Session, email: str, password: str):
@@ -48,41 +61,6 @@ def delete_user_by_email_password(db: Session, email: str, password: str):
     db.exec(query)
     db.commit()
     return user_db
-
-
-    # print(user.model_dump())
-    # # Crie o objeto User com os dados fornecidos
-    # db_user = models.User(user.model_dump())
-    # # print(db_user)
-
-    # # Adicione o usuário ao banco de dados
-    # db.add(db_user)
-    # db.commit()
-    # db.refresh(db_user)
-
-    # # Converta o objeto db_user em um dicionário
-    # # user_dict = db_user.dict()
-
-    # # Remova os campos 'password' e 'email' do dicionário
-    # # user_dict.pop('password', None)
-    # # user_dict.pop('email', None)
-
-    # # Crie um objeto de esquema User para retornar
-    # return db_user
-
-
-
-
-
-
-
-
-    # db_user = models.User(name=user.name, cpf=user.cpf, email=user.email, cargo=user.cargo, photo=user.photo, description=user.description, password=user.password)
-
-    # db.add(db_user)
-    # db.commit()
-    # db.refresh(db_user)
-    # return db_user
 
 
 # def get_items(db: Session, skip: int = 0, limit: int = 100):
