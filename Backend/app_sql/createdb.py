79074591938ea6c8dbe6cd8a2b84f437a57a5d94
Willians <09willians@gmail.com>
2024-05-20 11:08:18 -0300

    feat: criação de cargos

--- Backend/app_sql/createdb.py
+++ Backend/app_sql/createdb.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session
 from schemas import Cargo, IntermediariaUserProducts, Products, ImagesProducts, IntermediariaUserServices, Service, ImagesService, User, Tecnologia, UserTecnologia, RedesSociais, UserRedesSociais
 
 sqlite_file_name = "database.db"
@@ -7,3 +7,22 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url)
 
 SQLModel.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    session = Session(engine)
+
+    adm = Cargo(name="Administrador", description="Profissional responsável por gerenciar e administrar um sistema, garantindo que ele funcione corretamente e que as informações estejam seguras.")
+    session.add(adm)
+    session.commit()
+
+    frontend = Cargo(name="Desenvolvedor Frontend", description="Profissional responsável por criar e implementar a interface de usuário de sites e aplicativos web, garantindo uma experiência visual e funcional intuitiva.")
+    session.add(frontend)
+    session.commit()
+
+    backend = Cargo(name="Desenvolvedor Backend", description="Profissional responsável por criar e implementar a lógica de um sistema, garantindo que ele funcione corretamente e que as informações estejam seguras.")
+    session.add(backend)
+    session.commit()
+
+    fullstack = Cargo(name="Desenvolvedor Fullstack", description="Profissional responsável por criar e implementar a interface de usuário e a lógica de um sistema, garantindo que ele funcione corretamente e que as informações estejam seguras.")
+    session.add(fullstack)
+    session.commit()
